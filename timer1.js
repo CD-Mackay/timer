cd4dd87ba621e520db34d3284b4b1f9a29217634
CD-Mackay <connor.mackay@gmail.com>
2020-12-01 17:57:43 +0000

    commented

--- timer1.js
+++ timer1.js
@@ -2,10 +2,10 @@ let numbers = process.argv.slice(2);
 const alarm = function(numbers) {
   for (let i = 0; i < numbers.length; i++) {
     numbers[i] = parseInt(numbers[i]);
-    if (!isNaN(numbers[i]) && numbers[i] > 0)
+    if (!isNaN(numbers[i]) && numbers[i] > 0) //skip Nan entries as well as negative entries.
       setTimeout(() => {
         process.stdout.write("BEEP BEEP(sound was not working)!\n");
-      }, numbers[i] * 1000);
+      }, numbers[i] * 1000); // increment timer based on input
   }
 
 };
